87c54ff6fdf2e9567355b9e6c04d095a4f9bb1f0
Seshu1729 <Seshu1729@missionrnd.com>
2016-03-25 10:55:18 +0530

    Submit Commit

--- src/SortedArraytoBST.cpp
+++ src/SortedArraytoBST.cpp
@@ -27,15 +27,42 @@ Note : Return Null for invalid Inputs
 Note : Donot create a new BST .
 */
 #include <stdlib.h>
-struct node{
+struct node
+{
 	struct node * left;
 	int data;
 	struct node *right;
 };
 
+struct node *get_node()
+{
+	struct node *new_node;
+	new_node = (struct node *) malloc(sizeof(struct node));
+	new_node->left = NULL;
+	new_node->right = NULL;
+	return new_node;
+}
 
-struct node * convert_array_to_bst(int *arr, int len){
-	
-	return NULL;
+struct node *convert_array_to_bst_main(int *arr, int start, int end)
+{
+	int mid;
+	struct node *new_node = NULL;
+	if (start<end)
+	{
+		mid = (start + end) / 2;
+		new_node = get_node();
+		new_node->data = arr[mid];
+		new_node->left = convert_array_to_bst_main(arr, start, mid);
+		new_node->right = convert_array_to_bst_main(arr, mid + 1, end);
+	}
+	return new_node;
+}
+
+struct node * convert_array_to_bst(int *arr, int len)
+{
+	if (arr == NULL)
+		return NULL;
+	else
+		return convert_array_to_bst_main(arr, 0, len);
 }
 
