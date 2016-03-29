89f1508f9e9a058d935acad16017ccb8d18a36e0
Seshu1729 <Seshu1729@missionrnd.com>
2016-03-29 18:38:25 +0530

    Submit Commit

--- src/BSTClosestLeafDistance.cpp
+++ src/BSTClosestLeafDistance.cpp
@@ -33,13 +33,57 @@ Return -1 ,for Invalid Inputs
 #include <stdlib.h>
 #include <stdio.h>
 
-struct node{
-  struct node * left;
-  int data;
-  struct node *right;
+struct node
+{
+    struct node * left;
+    int data;
+    struct node *right;
 };
+
+int min(int a,int b)
+{
+    if(a<b)
+        return a;
+    return b;
+}
+
+int get_minimum_distance(struct node *root)
+{
+    int min_height_of_left_subtree, min_height_of_right_subtree;
+    if(root==NULL)
+        return 0;
+    else if (root->left == NULL && root->right==NULL)
+        return 0;
+    else if(root->left==NULL)
+        return 1 + get_minimum_distance(root->right);
+    else if(root->right==NULL)
+        return 1 + get_minimum_distance(root->left);
+    else
+    {
+        min_height_of_right_subtree = get_minimum_distance(root->right);
+        min_height_of_left_subtree = get_minimum_distance(root->left);
+        return 1 + min(min_height_of_right_subtree,min_height_of_left_subtree);
+    }
+}
+
+int get_closest_leaf_distance_recursively(struct node *root, struct node *temp,int min_distance)
+{
+    int current_min_distance;
+    current_min_distance = min(get_minimum_distance(root),min_distance);
+    if(root==temp)
+        return current_min_distance;
+    else if(root->data<temp->data)
+        return get_closest_leaf_distance_recursively(root->right,temp,current_min_distance+1);
+    else
+        return get_closest_leaf_distance_recursively(root->left,temp,current_min_distance+1);
+}
 
 int get_closest_leaf_distance(struct node *root, struct node *temp)
 {
-  return -1;
+    int result,min_distance;
+    if(root==NULL||temp==NULL)
+        return -1;
+    min_distance = get_minimum_distance(root);
+    result = get_closest_leaf_distance_recursively(root,temp,min_distance);
+    return result;
 }
